6070e044c0a9d080cbb56e10e2685cb22da26503
GuillermoMorales <00011417@uca.edu.sv>
2019-04-12 08:51:12 -0600

    Landscape

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), Fragmento.CajitaFeliz {
@@ -14,6 +16,11 @@ class MainActivity : AppCompatActivity(), Fragmento.CajitaFeliz {
 
         val fragmento=Fragmento.newInstance("Hardcore","text")
         supportFragmentManager.beginTransaction().add(R.id.activity_content,fragmento).commit();
+
+        if(resources.configuration.orientation==Configuration.ORIENTATION_LANDSCAPE)
+        {
+            Toast.makeText(this,"Esta en landscape",Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun clickListenerHandler(view: TextView)
